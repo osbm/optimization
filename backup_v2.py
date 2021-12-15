84568282d226c8b84b64b8e3a9551b431001cd7b
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2021-12-16 02:26:32 +0300

    Update backup_v2.py

--- backup_v2.py
+++ backup_v2.py
@@ -3,6 +3,17 @@ import time
 from datetime import datetime
 import subprocess
 
+assert os.name == "posix"  # only works on linux
+
+user = "osman"
+homeFolder = "/home/" + user
+
+
+folders = {
+    "Files": homeFolder + "Files",
+    "Workplace": homeFolder + "Workplace",
+    "Pictures": homeFolder + "Pictures",
+}
 
 def notify(title, message):
     print(subprocess.run(["notify-send", title, message]))
